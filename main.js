45ecf3f69b0f86f038471dbd55794c87549f54c7
NathanaelReges <35698221+NathanaelReges@users.noreply.github.com>
2018-04-12 20:09:34 -0300

    upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const testAutoResize = scripts['tools/testAutoResizeTextArea.js']
+
+
+requestAnimationFrame(testAutoResize)
+
+
+
+
+/*
 # autoResizeTextArea
 
 const  **module** =  autoResizeTextArea({**ele**, **minHeight**, **maxHeight**})<br/>
@@ -8,3 +17,4 @@ const  **module** =  autoResizeTextArea({**ele**, **minHeight**, **maxHeight**})
 <br/> &ensp;&ensp;**module.onTypedNewLine** &ensp; If the page is scrollable and the user is typing a new line out of the viewport the browser by default scrolls down so the line which the user is in becomes visible. You might want to scroll a little more to show some related button or to center correctly the bottom of the element. When this happens this module will call it's onTypedNewLine method and you can assign a function to it and make the adjustments that you need.
 <br/> &ensp;&ensp;**module.reset()** &ensp;  It resets the height of the element to the minHeight. You can use it if you have emptied the textAreaElement.value and want that the height is restored to the min.
 <br/> &ensp;&ensp;**module.forceTest()** &ensp;  Retest the element height.
+*/
---
+++ tools/autoResizeTextArea.js
@@ -0,0 +1,77 @@
+scripts['autoResizeTextArea.js'] = function autoResizeTextArea ({ele, minHeight, maxHeight}) {
+
+    const module = {}
+    let lastHeight = 0
+    
+    //If the text area overflow isn't hidden it won't work.
+    ele.style.overflow = 'hidden'
+
+    ele.addEventListener('keydown', test)
+
+
+
+    module.forceTest = test
+
+    module.reset = () => {
+        ele.style.height = minHeight + "px";
+    }
+
+    return module
+
+
+
+    function test () {
+
+        requestAnimationFrame(() => {
+            
+            let eleScrolled = false
+
+            //If the page is scrollable and the user is typing a new line out of the viewport
+            //the browser by default scrolls down so the line which the user is in becomes visible.
+            //You might want to scroll a little more to show some related button or to center correctly
+            //the bottom of the element.
+            //When this happens this module will call it's onTypedNewLine method and you can assign a function
+            //to it and make the adjustments that you need.
+            if(module.onTypedNewLine){
+                if(ele.scrollTop != 0) 
+                    eleScrolled = true
+            }
+
+            
+            //Force scrollHeight to appear
+            ele.style.height = "10px" 
+
+
+            //Add margin so the space that the element takes on the page doesn't suddenly jumps to 10px 
+            //and then to the adjusted height, what could possibly mess up the scrollTop of the page.
+            //That's the reason why we need to keep the lastHeight Value.
+            ele.style.marginBottom = lastHeight - 10 + 'px' 
+                
+
+            //scrollHeight = height of the content
+            let newHeight = ele.scrollHeight
+            if(minHeight && newHeight < minHeight) newHeight = minHeight
+            if(maxHeight && newHeight > maxHeight) newHeight = maxHeight
+
+
+            ele.style.height = newHeight + "px";
+            ele.style.marginBottom = ''
+            
+            lastHeight = newHeight
+
+
+            if(eleScrolled){
+                module.onTypedNewLine()
+            }
+            
+            if(module.callBack){
+                module.callBack()
+            }
+
+        })
+
+    }
+
+}
+
+
